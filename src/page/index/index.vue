5bf333434be399f39a1aeecedeb80c15ac5be651
mumuxiwang <wangmumuxi521@163.com>
2017-12-16 23:28:26 +0800

    xiedaoxiaotubiaole

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -1,15 +1,15 @@
 <template>
 	<div>
       <header class="header">
-      	<div class="back">返回</div>
-      	<div class="search"></div>
-      	<div class="city">城市</div>
+      	<div class="back iconfont">&#xe624;</div>
+      	<div class="search"><a href="#" class="prompt iconfont">&#xe86e;输入城市/景点/游玩主题</a></div>
+      	<div class="city iconfont">城市&#xe600;</div>
       </header>
 
       <swiper :options="swiperOption">
-        <swiper-slide>
+        <swiper-slide v-for="item in swiperInfo" :key="item.id">
           <div class="swiper-img-con">
-            <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1609/15/630b82d932a3c402.jpg_640x200_862e836b.jpg"/>
+            <img class="swiper-img" :src="item.imgUrl"/>
           </div>	
         </swiper-slide>
         <swiper-slide>
@@ -19,18 +19,37 @@
         </swiper-slide>
         <div class="swiper-pagination"  slot="pagination"></div>
       </swiper>
+      <div>123123</div>
 
 	</div>
 </template>
 <script>
 export default {
+
   name: 'Index',
   data () {
     return {
+      swiperInfo: [],
       swiperOption: {
-        autoplay: 1000,
-        direction: 'horizontal'
+        loop: true,
+        pagination: '.swiper-pagination',
+        autoplay: 2000
       }
+    }
+  },
+
+  created () {
+    this.getIndexData()
+  },
+  methods: {
+
+    getIndexData () {
+      this.$http.get('./static/index.json')
+        .then(this.handleGetDataSucc.bind(this))
+    },
+
+    handleGetDataSucc (res) {
+      this.swiperInfo = res.body.data.swiper
     }
   }
 }
@@ -44,6 +63,7 @@ export default {
   .back {
   	width: .64rem;
   	line-height: .86rem;
+    text-align: center;
   }
   .search {
   	flex: 1;
@@ -51,13 +71,22 @@ export default {
   	background: #fff;
   	border-radius: .1rem;
   }
+  .prompt {
+    font-size: 0.26rem;
+    color: #ccc;
+    line-height: 0.6rem;
+    padding-left:0.15rem;
+  }
   .city {
   	width: 1.14rem;
   	line-height: .86rem;
   	text-align: center;
   }
   .swiper-img-con {
+  	overflow: hidden;
     width: 100%;
+    height: 0;
+    padding-bottom: 31.25%;
   }
   .swiper-img {
   	width: 100%;
